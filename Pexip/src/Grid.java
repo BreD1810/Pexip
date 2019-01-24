f31f45d147e4ba4d98309d6703855032f526911a
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2019-01-24 13:25:24 +0000

    Added methods to check for characters below and to the right of a location. Added a method to get the locations of a specific character.

--- Pexip/src/Grid.java
+++ Pexip/src/Grid.java
@@ -34,4 +34,32 @@ public class Grid
         }
     }
 
+    public ArrayList<Integer> getCharLocations(char character) {return charLocations.get(character);}
+
+    /**
+     * @param location The location of the character being queried
+     * @param noBelow The number of positions below the queried character
+     * @return The character in the requested position - Returns a space if invalid
+     */
+    public char getCharBelow(int location, int noBelow)
+    {
+        int position = location + (noBelow * ROW_LENGTH);
+        if(location < 0 || location > contents.length() || noBelow < 1 || position >= contents.length())
+            return ' ';
+        return contents.charAt(position);
+    }
+
+    /**
+     * @param location The location of the character being queried
+     * @param noRight The number of positions to the right of the queried character
+     * @return The character in the requested position - Returns a space if invalid
+     */
+    public char getCharRight(int location, int noRight)
+    {
+        int noLeftOnRow = (Math.round(location/ROW_LENGTH) + 1) * ROW_LENGTH - location - 1;
+        if(location < 0 || location > contents.length() || noRight < 1 || noRight > noLeftOnRow)
+            return ' ';
+        return contents.charAt(location + noRight);
+    }
+
 }
