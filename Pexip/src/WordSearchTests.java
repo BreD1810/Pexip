640b90ae817d2b1b89c82bb51d2a64091b9f095b
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2019-01-29 10:56:07 +0000

    Created tests for a grid as large as the one specified in the spec

--- Pexip/src/WordSearchTests.java
+++ Pexip/src/WordSearchTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,14 +10,120 @@ import static org.junit.jupiter.api.Assertions.*;
 public class WordSearchTests
 {
 
-    private Grid smallGrid;
-    private WordSearch smallGridSearch;
+    private static final int ROW_LENGTH = 10000;
+    private static String largeGridString;
+    private static Grid smallGrid, largeGrid;
+    private static WordSearch smallGridSearch, largeGridSearch;
+    private static ArrayList<String> words;
+    private static Random rand = new Random();
 
-    @BeforeEach
-    void setUp()
+    @BeforeAll
+    static void setUp()
     {
         smallGrid = new Grid("aaabccbbc");
         smallGridSearch = new WordSearch(smallGrid);
+        largeGrid = generateLargeGrid();
+        largeGridSearch = new WordSearch(largeGrid);
+        words = generateWords();
+    }
+
+    private static Grid generateLargeGrid()
+    {
+        Random rand = new Random();
+        StringBuilder gridString = new StringBuilder();
+
+        for(int i = 0; i < ((ROW_LENGTH * ROW_LENGTH) - 9); i++)
+            gridString.append((char)(rand.nextInt(26) + 'a'));
+
+        //"testwords" going right in a valid position
+        int position = rand.nextInt(ROW_LENGTH*ROW_LENGTH) - 9;
+        int noLeftOnRow = (Math.round(position/ROW_LENGTH) + 1) * ROW_LENGTH - position - 1;
+        while(noLeftOnRow <= 9)
+        {
+            position = rand.nextInt(ROW_LENGTH*ROW_LENGTH) - 9;
+            noLeftOnRow = (Math.round(position/ROW_LENGTH) + 1) * ROW_LENGTH - position - 1;
+        }
+        gridString.insert(position, "testwords");
+
+        //"correctword" going down in a valid position
+        //Length 11
+        String word = "correctword";
+        position = rand.nextInt(ROW_LENGTH * ROW_LENGTH);
+        while(position + (11 * ROW_LENGTH) > (ROW_LENGTH * ROW_LENGTH))
+            position = rand.nextInt(ROW_LENGTH * ROW_LENGTH);
+        for(int i = 0; i < word.length(); i++)
+        {
+            int charPosition = position + (i * ROW_LENGTH);
+            gridString.replace(charPosition, charPosition, String.valueOf(word.charAt(i)));
+        }
+
+        largeGridString = gridString.toString();
+        return new Grid(gridString.toString());
+    }
+
+    private static ArrayList<String> generateWords()
+    {
+        ArrayList<String> words = new ArrayList<>();
+        for(int i = 0; i < 1000000; i++)
+        {
+            if(rand.nextBoolean())
+                words.add(generateRightWord());
+            else
+                words.add(generateDownWord());
+        }
+        return words;
+    }
+
+    private static String generateRightWord()
+    {
+        int wordLength = rand.nextInt(25) + 1;
+        int row = rand.nextInt(ROW_LENGTH);
+        int column = rand.nextInt(ROW_LENGTH - wordLength);
+        int position = (ROW_LENGTH * row) + column;
+        return largeGridString.substring(position, position + wordLength + 1);
+    }
+
+    private static String generateDownWord()
+    {
+        StringBuilder word = new StringBuilder();
+        int wordLength = rand.nextInt(25) + 1;
+        int row = rand.nextInt(ROW_LENGTH - wordLength);
+        int column = rand.nextInt(ROW_LENGTH);
+        int position = (ROW_LENGTH * row) + column;
+        for(int i = 0; i < wordLength; i++)
+        {
+            word.append(largeGridString.charAt(position + (i * ROW_LENGTH)));
+        }
+        return word.toString();
+    }
+
+    private boolean largeGridContains(String word)
+    {
+        if(largeGridString.contains(word))
+        {
+            int location = largeGridString.indexOf(word);
+            int noLeftOnRow = (Math.round(location/ROW_LENGTH) + 1) * ROW_LENGTH - location - 1;
+            if(noLeftOnRow >= word.length())
+            {
+                return true;
+            }
+        }
+
+        int nextLetterPosition;
+        ArrayList<Integer> possibleWordStarts = largeGrid.getCharLocations(word.charAt(0));
+        for(int position : possibleWordStarts)
+        {
+            for(int i = 1; i < word.length(); i++)
+            {
+                nextLetterPosition = position + (i * ROW_LENGTH);
+                if(nextLetterPosition >= largeGridString.length() || !(largeGridString.charAt(nextLetterPosition) == word.charAt(i)))
+                    break;
+                if(i == word.length()-1)
+                    return true;
+            }
+        }
+
+        return false;
     }
 
     @Test
@@ -52,6 +159,12 @@ public class WordSearchTests
         assertEquals(' ', smallGrid.getCharRight(0, 3));
         assertEquals(' ', smallGrid.getCharRight(-1, 1));
         assertEquals(' ', smallGrid.getCharRight(1, -1));
+        assertEquals(' ', smallGrid.getCharRight(8, 1));
+        assertEquals(' ', smallGrid.getCharRight(9, 1));
+        assertTrue(smallGrid.checkWordRight("aaa", 0));
+        assertTrue(smallGrid.checkWordRight("aa", 1));
+        assertFalse(smallGrid.checkWordRight("aa", 2));
+        assertFalse(smallGrid.checkWordRight("aa", 3));
     }
 
     @Test
@@ -65,6 +178,29 @@ public class WordSearchTests
         assertFalse(smallGridSearch.is_present(""));
         assertFalse(smallGridSearch.is_present("abc"));
         assertFalse(smallGridSearch.is_present("accc"));
+    }
+
+    @Test
+    void largeGrid1WordRight()
+    {
+        assertTrue(largeGridSearch.is_present("testwords"));
+        assertTrue(largeGridContains("testwords"));
+        assertSame(largeGridContains("abcdefghi"), largeGridSearch.is_present("abcdefghi"));
+    }
+
+    @Test
+    void largeGrid1WordDown()
+    {
+        assertTrue(largeGridSearch.is_present("correctword"));
+        assertTrue(largeGridContains("correctword"));
+        assertSame(largeGridContains("abcdefghi"), largeGridSearch.is_present("abcdefghi"));
+    }
+
+    @Test
+    void largeWordNumber()
+    {
+        for (String word:words)
+            assertTrue(largeGridSearch.is_present(word));
     }
 
 }
